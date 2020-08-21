12bc9c8770ac7964d09b9131c8e2815336c047fc
Bryce Mara Jarrett <brycej98@gmail.com>
2020-08-21 15:12:49 -0500

    Add book

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+   }
+  return mainCharacter;
+};
+
+function saveReview(reviewText, reviews) {
+  if(!reviews.includes(reviewText)) {
+  reviews.push(reviewText);
 }
+};
+
+function calculatePageCount(title) {
+  return (title.length * 20);
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var newbook = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: (bookTitle.length * 20),
+    genre: genre,
+  }
+  return newbook
+}
+
+function editBook(book) {
+  book.pageCount = ( book.pageCount * .75 )
+  return book.pageCount
+};
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
