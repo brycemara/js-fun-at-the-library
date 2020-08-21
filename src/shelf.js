0c25920525ff036038b5bfdaed39b18d8ab2d846
Bryce Mara Jarrett <brycej98@gmail.com>
2020-08-21 15:13:09 -0500

    Add shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,38 @@
 
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+  shelf.unshift(book)
+}
+};
+
+function unshelfBook(title, shelf) {
+ for (var i = 0; i < shelf.length; i++)
+  if (shelf[i].title === title) {
+    shelf.splice(i, 1)
+  }
+};
+
+function listTitles(shelf) {
+  var string = [];
+  for (var i = 0; i < shelf.length; i++) {
+    string.push(shelf[i].title)
+  }
+   return string.join(', ');
+};
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+      return true
+    }
+}  return false
+};
+
+
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
