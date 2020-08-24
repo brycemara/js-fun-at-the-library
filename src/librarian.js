3ac14b9223d9f03c63fb00bb3c1c82386cc13969
Bryce Mara Jarrett <brycej98@gmail.com>
2020-08-24 13:55:40 -0500

    Add librarian with refactors

--- src/librarian.js
+++ src/librarian.js
@@ -10,6 +10,7 @@ class Librarian {
     this.name = name;
     this.library = library;
   }
+
   greetPatron(name2, morning) {
     this.name2 = name2;
     this.morning = morning;
@@ -19,24 +20,37 @@ class Librarian {
     return `Hello, ${this.name2}!`;
   }
 }
+
 findBook(book) {
-  for ( var i = 0; i < this.library.shelves.fantasy.length; i++) {
-    if (this.library.shelves.fantasy[i].title === book) {
-    this.library.shelves.fantasy.splice(i,1);
-    return `Yes, we have ${book}` }
-  }
-  for (var i = 0; i < this.library.shelves.nonFiction.length; i++) {
-    if (this.library.shelves.nonFiction[i].title === book) {
-    this.library.shelves.nonFiction.splice(i,1);
-    return `Yes, we have ${book}` }
-  }
-  for (var i=0; i < this.library.shelves.fiction.length; i++) {
-    if (this.library.shelves.fiction[i].title === book) {
-    this.library.shelves.fiction.splice(i,1);
-    return `Yes, we have ${book}` }
-  }
-  return `Sorry, we do not have ${book}`;
+//   for ( var i = 0; i < this.library.shelves.fantasy.length; i++) {
+//     if (this.library.shelves.fantasy[i].title === book) {
+//     this.library.shelves.fantasy.splice(i,1);
+//     return `Yes, we have ${book}` }
+//   }
+//   for (var i = 0; i < this.library.shelves.nonFiction.length; i++) {
+//     if (this.library.shelves.nonFiction[i].title === book) {
+//     this.library.shelves.nonFiction.splice(i,1);
+//     return `Yes, we have ${book}` }
+//   }
+//   for (var i=0; i < this.library.shelves.fiction.length; i++) {
+//     if (this.library.shelves.fiction[i].title === book) {
+//     this.library.shelves.fiction.splice(i,1);
+//     return `Yes, we have ${book}` }
+//   }
+//   return `Sorry, we do not have ${book}`;
+// }
+
+for (var genre in this.library.shelves) {
+  for (var i = 0; i < this.library.shelves[genre].length; i++) {
+    if (this.library.shelves[genre][i].title === book ) {
+      this.library.shelves[genre].splice(i,1);
+      return `Yes, we have ${book}`
+    }
+  } return `Sorry, we do not have ${book}`;
 }
+}
+
+
   calculateLateFee(daysLate) {
       return Math.ceil(daysLate * 0.25);
   }
