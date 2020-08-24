4acbd005d2c616cef60f7450b454f1325f8b3df4
Bryce Mara Jarrett <brycej98@gmail.com>
2020-08-24 13:55:18 -0500

    Add Library with refactors

--- src/Library.js
+++ src/Library.js
@@ -15,23 +15,48 @@ function addBook(libName, book) {
 };
 
 function checkoutBook(libName, book) {
-  for ( var i = 0; i < libName.shelves.fantasy.length; i++) {
-    if (libName.shelves.fantasy[i].title === book) {
-    libName.shelves.fantasy.splice(i,1);
-    return `You have now checked out ${book} from the ${libName.name}` }
-  }
-  for (var i = 0; i < libName.shelves.nonFiction.length; i++) {
-    if (libName.shelves.nonFiction[i].title === book) {
-    libName.shelves.nonFiction.splice(i,1);
-    return `You have now checked out ${book} from the ${libName.name}` }
-  }
-  for (var i=0; i < libName.shelves.fiction.length; i++) {
-    if (libName.shelves.fiction[i].title === book) {
-    libName.shelves.fiction.splice(i,1);
-    return `You have now checked out ${book} from the ${libName.name}` }
-  }
-  return `Sorry, there are currently no copies of ${book} available at the ${libName.name}`
-}
+//   for ( var i = 0; i < libName.shelves.fantasy.length; i++) {
+//     if (libName.shelves.fantasy[i].title === book) {
+//     libName.shelves.fantasy.splice(i,1);
+//     return `You have now checked out ${book} from the ${libName.name}` }
+//   }
+//   for (var i = 0; i < libName.shelves.nonFiction.length; i++) {
+//     if (libName.shelves.nonFiction[i].title === book) {
+//     libName.shelves.nonFiction.splice(i,1);
+//     return `You have now checked out ${book} from the ${libName.name}` }
+//   }
+//   for (var i=0; i < libName.shelves.fiction.length; i++) {
+//     if (libName.shelves.fiction[i].title === book) {
+//     libName.shelves.fiction.splice(i,1);
+//     return `You have now checked out ${book} from the ${libName.name}` }
+//   }
+//   return `Sorry, there are currently no copies of ${book} available at the ${libName.name}`
+// }
+
+
+for (var genre in libName.shelves) {
+  console.log(genre);
+  for ( var i = 0; i < libName.shelves[genre].length; i++) {
+      if (libName.shelves[genre][i].title === book) {
+      libName.shelves[genre].splice(i,1);
+      return `You have now checked out ${book} from the ${libName.name}` }
+    }
+} return `Sorry, there are currently no copies of ${book} available at the ${libName.name}`
+};
+
+
+
+
+// const object = { a: 1, b: 2, c: 3 };
+//
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+//
+
+
+
+
 
 module.exports = {
   createLibrary,
